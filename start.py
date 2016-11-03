5c7a0fd433bafda3d86460654aa123204fa1f73c
Matthew Bennett-Lovesey <matthew@quickwebdesign.net>
2016-11-03 20:09:32 +0000

    Initial commit: let's get busy

---
+++ start.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+
+
+from adventurelib import *
+
+def walk(x, y):
+    print('Walking up %s and right %s...' % (x, y))
+
+@when('walk north')
+def walk_north():
+    walk(0, 1)
+
+@when('walk east')
+def walk_east():
+    walk(1, 0)
+
+@when('walk south')
+def walk_south():
+    walk(0, -1)
+
+@when('walk west')
+def walk_west():
+    walk(-1, 0)
+
+@when('despair')
+def despair():
+    print("Waaaaaahhhhhhh!!!")
+
+
+
+@when('take THING')
+def take(thing):
+    print('You take the %s.' % thing)
+    inventory.append(thing)
+
+
+@when('brexit')
+def brexit():
+    print("Geee, thanks...")
+    exit()
+
+
+
+
+start()
+
+
